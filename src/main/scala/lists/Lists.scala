a8ee6e7410ee7a619431ea91d810f4d713f1be98
Wojciech Langiewicz <wlangiewicz@gmail.com>
2017-07-02 21:16:44 +0200

    P03

--- src/main/scala/lists/Lists.scala
+++ src/main/scala/lists/Lists.scala
@@ -6,14 +6,21 @@ object Lists {
   @tailrec
   def last[A](list: List[A]): A = list match {
     case h :: Nil => h
-    case _ :: t => last(t)
-    case _ => throw new IllegalArgumentException("List must not be empty")
+    case _ :: t   => last(t)
+    case _        => throw new IllegalArgumentException("List must not be empty")
   }
 
   @tailrec
   def penultimate[A](list: List[A]): A = list match {
     case f :: _ :: Nil => f
-    case _ :: t => penultimate(t)
-    case _ => throw new IllegalArgumentException("List must contain at least 2 elements")
+    case _ :: t        => penultimate(t)
+    case _             => throw new IllegalArgumentException("List must contain at least 2 elements")
+  }
+
+  @tailrec
+  def nth[A](index: Int, list: List[A]): A = (index, list) match {
+    case (n, l) if n > 0 && l.isEmpty => throw new IllegalArgumentException("n must be < list length")
+    case (0, h :: _)                  => h
+    case (n, _ :: t)                  => nth(n - 1, t)
   }
 }
--- src/test/scala/lists/ListsTest.scala
+++ src/test/scala/lists/ListsTest.scala
@@ -6,11 +6,16 @@ class ListsTest extends FlatSpec with Matchers {
   import Lists._
   "last" should "work" in {
     last(List(1, 1, 2, 3, 5, 8)) shouldBe 8
-    an [IllegalArgumentException] should be thrownBy last(List.empty)
+    an[IllegalArgumentException] should be thrownBy last(List.empty)
   }
 
   "penultimate" should "work" in {
     penultimate(List(1, 1, 2, 3, 5, 8)) shouldBe 5
-    an [IllegalArgumentException] should be thrownBy penultimate(List(1))
+    an[IllegalArgumentException] should be thrownBy penultimate(List(1))
+  }
+
+  "nth" should "work" in {
+    nth(2, List(1, 1, 2, 3, 5, 8)) shouldBe 2
+    an[IllegalArgumentException] should be thrownBy nth(3, List(1))
   }
 }
